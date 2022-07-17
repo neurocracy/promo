08e4e7c709258ae206524d3bf46278c5b97e9ec8
Matei Stanca <i@ambientimpact.com>
2022-07-17 03:56:28 -0400

    public/javascript/media.js: Refactored; only show current colour scheme:
    This means that the PhotoSwipe gallery that opens only contains the
    gallery items matching the colour scheme of the link that was clicked.
    I.e. if you click one of the light scheme links, it will only show and
    cycle through light scheme items, and the same for dark colour scheme
    links.

--- public/javascript/media.js
+++ public/javascript/media.js
@@ -16,6 +16,13 @@
   }
 
   /**
+   * The colour scheme attribute name on media links.
+   *
+   * @type {String}
+   */
+  const colourSchemeAttributeName = 'data-colour-scheme';
+
+  /**
    * The PhotoSwipe container element.
    *
    * @type {HTMLELement}
@@ -23,157 +30,248 @@
   const photoswipeElement = document.querySelector('.pswp');
 
   /**
-   * All media links in the features section.
+   * All features media sections.
    *
    * @type {NodeList}
    */
-  const itemLinks = document.querySelectorAll('.features-section__media a');
+  const mediaSections = document.querySelectorAll('.features-section__media');
 
   /**
-   * PhotoSwipe gallery items.
+   * Flat array of all media links that have been added to a PhotoSwipe gallery.
    *
    * @type {Array}
    */
-  let items = [];
+  const mediaLinks = [];
 
-  for (let i = 0; i < itemLinks.length; i++) {
+  /**
+   * PhotoSwipe gallery items categorized by dark or light colour scheme.
+   *
+   * Note that the top-level keys defined here are used to validate link colour
+   * scheme attribute values. Only the top-level keys already defined here will
+   * have gallery items.
+   *
+   * @type {Object}
+   *
+   * @see getLinkColourScheme()
+   *   Uses the top-level keys here to validate a link's colour scheme attribute
+   *   value.
+   */
+  let galleryItems = {dark: [], light: []};
 
-    items.push({
-      el:   itemLinks[i],
-      src:  itemLinks[i].href,
-      msrc: itemLinks[i].getElementsByTagName('img')[0].getAttribute('src'),
-      w:    itemLinks[i].getAttribute('data-photoswipe-linked-width'),
-      h:    itemLinks[i].getAttribute('data-photoswipe-linked-height')
-    });
+  /**
+   * Get a link's colour scheme value.
+   *
+   * @param {HTMLAnchorElement} link
+   *   The link element.
+   *
+   * @return {String|null}
+   *   The validated colour scheme name or null if it did not pass validation or
+   *   the attribute doesn't exist.
+   */
+  function getLinkColourScheme(link) {
 
-    itemLinks[i].setAttribute('data-photoswipe-gallery-index', i);
+    const colourScheme = link.getAttribute(colourSchemeAttributeName);
+
+    if (colourScheme === null || !galleryItems.hasOwnProperty(colourScheme)) {
+
+      console.warn('Invalid colour scheme found. ', link, colourScheme);
+
+      return null;
+
+    }
+
+    return colourScheme;
 
   }
 
-  if (items.length === 0) {
-    return;
-  }
+  /**
+   * Build a PhotoSwipe gallery item for the provided link.
+   *
+   * @param {HTMLAnchorElement} link
+   *   The link element.
+   *
+   * @return {Object}
+   */
+  function buildGalleryItem(link) {
 
-  for (let i = 0; i < itemLinks.length; i++) {
+    return {
+      el:   link,
+      src:  link.href,
+      msrc: link.getElementsByTagName('img')[0].getAttribute('src'),
+      w:    link.getAttribute('data-photoswipe-linked-width'),
+      h:    link.getAttribute('data-photoswipe-linked-height')
+    };
 
-    itemLinks[i].addEventListener('click', function(event) {
+  };
 
-      // Don't do anything and defer to the default action if a modifier key
-      // was pressed during the click (to open the link in a new tab, window,
-      // etc.) - note that this is a truthy check rather than a strict check
-      // for the existence of and boolean true value of the letious event
-      // properties:
-      // * https://ambientimpact.com/web/snippets/conditional-statements-and-truthy-values-robust-client-side-javascript
-      // * https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/ctrlKey
-      // * https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/shiftKey
-      if (event.ctrlKey || event.shiftKey) {
-        return;
+  for (let i = 0; i < mediaSections.length; i++) {
+
+    /**
+     * All  links in this media section.
+     *
+     * @type {NodeList}
+     */
+    const links = mediaSections[i].getElementsByTagName('a');
+
+    for (let j = 0; j < links.length; j++) {
+
+      /**
+       * The link's colour scheme.
+       *
+       * @type {String|null}
+       */
+      const colourScheme = getLinkColourScheme(links[j]);
+
+      if (colourScheme === null) {
+        continue;
       }
 
-      /**
-       * The link element for this gallery index that was clicked.
-       *
-       * @type {HTMLAnchorElement}
-       */
-      const link = this;
+      galleryItems[colourScheme].push(buildGalleryItem(links[j]));
 
-      /**
-       * PhotoSwipe gallery options for this gallery.
-       *
-       * @type {Object}
-       */
-      let galleryOptions = {
-        index:      parseInt(
-          this.getAttribute('data-photoswipe-gallery-index')
-        ),
-        shareEl:    false,
-        galleryUID: 'features',
+      // Set the gallery index to the index of the media section, not the index
+      // of the link.
+      links[j].setAttribute('data-photoswipe-gallery-index', i);
+
+      mediaLinks.push(links[j]);
+
+    }
+
+  }
+
+  function itemEventHandler(event) {
+
+    // Don't do anything and defer to the default action if a modifier key
+    // was pressed during the click (to open the link in a new tab, window,
+    // etc.) - note that this is a truthy check rather than a strict check
+    // for the existence of and boolean true value of the letious event
+    // properties:
+    // * https://ambientimpact.com/web/snippets/conditional-statements-and-truthy-values-robust-client-side-javascript
+    // * https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/ctrlKey
+    // * https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/shiftKey
+    if (event.ctrlKey || event.shiftKey) {
+      return;
+    }
+
+    /**
+     * The link element for this gallery index that was clicked.
+     *
+     * @type {HTMLAnchorElement}
+     */
+    const link = this;
+
+    /**
+     * The link's colour scheme.
+     *
+     * @type {String|null}
+     */
+    const colourScheme = getLinkColourScheme(link);
+
+    if (colourScheme === null) {
+      return;
+    }
+
+    /**
+     * PhotoSwipe gallery options for this gallery.
+     *
+     * @type {Object}
+     */
+    let galleryOptions = {
+      index:      parseInt(
+        this.getAttribute('data-photoswipe-gallery-index')
+      ),
+      shareEl:    false,
+      galleryUID: 'features',
+    };
+
+    // This only provides the thumbnail callback if reduced motion is not
+    // specified. If the thumbnail callback isn't set, PhotoSwipe will not do
+    // the zoom transition in and out on open and close.
+    if (!window.matchMedia('(prefers-reduced-motion)').matches) {
+
+      galleryOptions.getThumbBoundsFn = function(index) {
+
+        /**
+         * The thumbnail image element for this gallery item.
+         *
+         * @type {HTMLImageElement}
+         */
+        const thumbnail = galleryItems[colourScheme][index].el.getElementsByTagName('img')[0];
+
+        /**
+         * Current vertical scroll of the viewport in pixels.
+         *
+         * @type {Number}
+         */
+        const pageYScroll = (
+          window.pageYOffset || document.documentElement.scrollTop
+        );
+
+        /**
+         * Thumbnail element DOMRect object.
+         *
+         * @type {DOMRect}
+         */
+        const rect = thumbnail.getBoundingClientRect();
+
+        return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
+
       };
 
-      // This only provides the thumbnail callback if reduced motion is not
-      // specified. If the thumbnail callback isn't set, PhotoSwipe will not do
-      // the zoom transition in and out on open and close.
-      if (!window.matchMedia('(prefers-reduced-motion)').matches) {
+    // If reduced motion is specified, tell PhotoSwipe to do a fade in and out
+    // on open and close, so that there's still some smoothness to the UX even
+    // without the zoom transitions.
+    } else {
+      galleryOptions.showHideOpacity = true;
+    }
 
-        galleryOptions.getThumbBoundsFn = function(index) {
+    /**
+     * PhotoSwipe instance for this gallery.
+     *
+     * @type {PhotoSwipe}
+     */
+    let gallery = new PhotoSwipe(
+      photoswipeElement, PhotoSwipeUI_Default,
+      // Initialize with the clicked (current) colour scheme items only.
+      galleryItems[colourScheme],
+      galleryOptions
+    );
 
-          /**
-           * The thumbnail image element for this gallery item.
-           *
-           * @type {HTMLImageElement}
-           */
-          const thumbnail = items[index].el.getElementsByTagName('img')[0];
+    /**
+     * The ally.js focus disabled instance handle, if active.
+     *
+     * @type {Object|undefined}
+     *
+     * @see https://allyjs.io/api/maintain/disabled.html
+     */
+    let focusDisabledHandle;
 
-          /**
-           * Current vertical scroll of the viewport in pixels.
-           *
-           * @type {Number}
-           */
-          const pageYScroll = (
-            window.pageYOffset || document.documentElement.scrollTop
-          );
-
-          /**
-           * Thumbnail element DOMRect object.
-           *
-           * @type {DOMRect}
-           */
-          const rect = thumbnail.getBoundingClientRect();
-
-          return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
-
-        };
-
-      // If reduced motion is specified, tell PhotoSwipe to do a fade in and out
-      // on open and close, so that there's still some smoothness to the UX even
-      // without the zoom transitions.
-      } else {
-        galleryOptions.showHideOpacity = true;
-      }
-
-      /**
-       * PhotoSwipe instance for this gallery.
-       *
-       * @type {PhotoSwipe}
-       */
-      let gallery = new PhotoSwipe(
-        photoswipeElement, PhotoSwipeUI_Default, items, galleryOptions
-      );
-
-      /**
-       * The ally.js focus disabled instance handle, if active.
-       *
-       * @type {Object|undefined}
-       *
-       * @see https://allyjs.io/api/maintain/disabled.html
-       */
-      let focusDisabledHandle;
-
-      gallery.listen('initialZoomInEnd', function() {
-        focusDisabledHandle = ally.maintain.disabled({
-          filter: '.pswp *'
-        });
+    gallery.listen('initialZoomInEnd', function() {
+      focusDisabledHandle = ally.maintain.disabled({
+        filter: '.pswp *'
       });
-
-      // This moves focus to the corresponding link containing the thumbnail of
-      // the current slide when PhotoSwipe closes, so that keyboard navigation
-      // doesn't get reset to the top of the page. Ugh. Note that we're doing
-      // this right when PhotoSwipe begins to close rather than on destroy, so
-      // that we don't risk anything else getting focus for a brief moment and
-      // potentially confusing or annoying assistive software users.
-      gallery.listen('initialZoomOut', function() {
-        focusDisabledHandle.disengage();
-        gallery.currItem.el.focus();
-      });
-
-      gallery.init();
-
-      // Always prevent the default link action at the end of the handler, in
-      // case of an exception in the preceding code.
-      event.preventDefault();
-
     });
 
+    // This moves focus to the corresponding link containing the thumbnail of
+    // the current slide when PhotoSwipe closes, so that keyboard navigation
+    // doesn't get reset to the top of the page. Ugh. Note that we're doing
+    // this right when PhotoSwipe begins to close rather than on destroy, so
+    // that we don't risk anything else getting focus for a brief moment and
+    // potentially confusing or annoying assistive software users.
+    gallery.listen('initialZoomOut', function() {
+      focusDisabledHandle.disengage();
+      gallery.currItem.el.focus();
+    });
+
+    gallery.init();
+
+    // Always prevent the default link action at the end of the handler, in
+    // case of an exception in the preceding code.
+    event.preventDefault();
+
+  };
+
+  for (let i = 0; i < mediaLinks.length; i++) {
+    mediaLinks[i].addEventListener('click', itemEventHandler);
   }
 
 })();
